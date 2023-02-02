3b14e6a50690a0112fc5262bf4b11547114990cf
kayfernander2022 <kayfernander@hotmail.com>
2023-02-02 10:29:33 -0500

    my first commit

---
+++ landscaper.py
@@ -0,0 +1,37 @@
+
+game = {"tool": 0, "money": 0}
+
+tools = [
+  {"name": "teeth", "profit": 1, "cost": 0 }
+]
+
+
+
+#Player functions
+
+def mow_lawn():
+  tool = tools[game["tool"]]
+  print(f"You have mowed the lawn with your {tool['name']} and made {tool['profit']}")
+  game["money"] += tool["profit"]
+
+
+
+
+while(True):
+  
+    user_choice = input("[1] Mow Lawn [2] Check Stats [3] Upgrade [4] Quit")
+    user_choice = int(user_choice)
+    
+    if(user_choice == 1):
+      mow_lawn()
+    
+    
+    if(user_choice == 2):
+      check_stats()
+    
+    if(user_choice == 3):
+      upgrade()
+    
+    if(user_choice == 4):
+      print("You have quit the game")
+      break
